c8bf03adc86e521fa2f03fcedfedb29c6a310729
Vlad15lav <vladislav.viryasov@gmail.com>
2025-10-15 12:55:56 +0300

    init commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='devsecops',
+    package_dir={'': 'src'}
+    packages=find_packages('src'),
+    version='0.1.0',
+    description='A short description of the project.',
+    author='vlad15lav',
+    license='MIT',
+)
